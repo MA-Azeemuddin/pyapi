a6ed1cb61fa847de738ba3afe477345048ad6ca5
MA-Azeemuddin <muhammad.azeemuddin@outlook.com>
2025-09-27 17:01:44 +0500

    CI-CD reconfig

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"This is Home route after CI/CD and no cache and git branch main"}
+    return {"After CI/CD v6 py github copilot"}
 
 @app.get("/api")
 def read_api():
